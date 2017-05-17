8c1354952c943b0282ae8952b1275d6e0889ab61
kyuwankim <lpnids@gmail.com>
2017-05-17 11:51:10 +0900

    showList() 추가

--- src/com/kyuwan/bbs/view/BbsList.java
+++ src/com/kyuwan/bbs/view/BbsList.java
@@ -1,30 +1,42 @@
 package com.kyuwan.bbs.view;
 
+import java.util.ArrayList;
+
+import com.kyuwan.bbs.model.Bbs;
+
 public class BbsList {
+	// 리스트에서 보여줄 저장소
+	ArrayList<Bbs> datas = new ArrayList<>();
 
 	// 목록 보여주기
-	public void showList(){
-		
+	public void showList() {
+		for (Bbs bbs : datas) {
+			System.out.print(bbs.getId() + " | ");
+			System.out.print(bbs.getTitle() + " | ");
+			System.out.print(bbs.getAuthor() + " | ");
+			System.out.print(bbs.getDate() + " | ");
+			System.out.println(bbs.getViewcount() + " | ");
+		}
 	}
-	
-	// 쓰기이동 
-	public void goInput(){
-		
+
+	// 쓰기이동
+	public void goInput() {
+
 	}
-	
-	// 상세보기 이동 
-	public void goDetail(long id){
-		
+
+	// 상세보기 이동
+	public void goDetail(long id) {
+
 	}
-	
+
 	// 검색
-	public void search(String word){
-		
+	public void search(String word) {
+
 	}
-	
+
 	// 삭제
-	public void delete(long id){
-		
+	public void delete(long id) {
+
 	}
-	
+
 }
