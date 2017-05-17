de0d768bd0bcd91adea290fea3597dc081cce29d
kyuwankim <lpnids@gmail.com>
2017-05-17 15:49:06 +0900

    Bbs Loader 에 Write 파일쓰기작성

--- src/com/kyuwan/bbs/model/BbsLoader.java
+++ src/com/kyuwan/bbs/model/BbsLoader.java
@@ -1,7 +1,13 @@
 package com.kyuwan.bbs.model;
 
-public class BbsLoader {
+import java.io.File;
+import java.io.IOException;
 
+public class BbsLoader {
+	final String DATABASE_DIR = "/Users/kimkyuwan/Desktop";
+	final String DATABASE_NAME = "bbs.db";
+	final String COLUMN_SEPERATOR = "^^"; // 한칸단위 
+	final String RECORD_SEPERATOR = "\r\n"; // 한줄단위 
 	/**
 	 * 저장소에서 데이터를 읽어옴
 	 * @return
@@ -16,9 +22,47 @@ public class BbsLoader {
 	
 	/**
 	 * 저장소에 데이터를 저장 
-	 * @param value
+	 * @param bbs 
 	 */
-	public void write(String value){
+	public void write(Bbs bbs){
+		// bbs에 있는 데이터를 텍스트 파일에 저장한다 
+		// 1. 디텍토리의 존재여부 검사
+		File dir = new File(DATABASE_DIR);
+		if(!dir.exists()){
+			dir.mkdirs();
+		}
+		
+		// 1.1 파일의 존재여부 검사
+		File database = new File(DATABASE_DIR+DATABASE_NAME);
+		if(!database.exists()){
+			try {
+				database.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		// 1.1 없으면 텍스트 파일 생성
+
+		
+		// 2. bbs의 내용을 database 파일에 저장을 한다
+		// 2.1 저장하는 데이터의 구조를 설계해야 한다 
+		// 2.1.1 구분자를 정의한다 
+		// 2.2 bbs의 내용을 횡으로 펼친다 
+		String oneDate = bbs.getId() + COLUMN_SEPERATOR + 
+							bbs.getTitle()  + COLUMN_SEPERATOR + 
+							bbs.getAuthor()  + COLUMN_SEPERATOR +
+							bbs.getDate()+ COLUMN_SEPERATOR +
+							bbs.getViewcount() + COLUMN_SEPERATOR +
+							bbs.getContent() + RECORD_SEPERATOR ;
+		
+		// 3. 행으로 펼쳐진 데이터를 파일의 제일 마지막줄에 저장한다 
+		
+		
+		
+		
+		
 		
 	}
 	
