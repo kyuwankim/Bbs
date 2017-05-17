a28fdcff9fed99667fd6229b8a3683767accda4b
kyuwankim <lpnids@gmail.com>
2017-05-17 11:57:39 +0900

    showDetail 추가

--- src/com/kyuwan/bbs/view/BbsDetail.java
+++ src/com/kyuwan/bbs/view/BbsDetail.java
@@ -1,5 +1,17 @@
 package com.kyuwan.bbs.view;
 
+import com.kyuwan.bbs.model.Bbs;
+
 public class BbsDetail {
+
+	public void showDetail(Bbs bbs) {
+		System.out.println(" 글번호 : " + bbs.getId());
+		System.out.println(" 제목 : " + bbs.getTitle());
+		System.out.println(" 작성자 : " + bbs.getAuthor());
+		System.out.println(" 날자 : " + bbs.getDate());
+		System.out.println(" 조회수 : " + bbs.getViewcount());
+		System.out.println(" 내용 : " + bbs.getContent());
+
+	}
 
 }
