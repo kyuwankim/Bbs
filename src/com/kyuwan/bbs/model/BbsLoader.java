3e9ce72b058ef6d46c5d25eeb5599bb542650867
kyuwankim <lpnids@gmail.com>
2017-05-17 11:32:27 +0900

    BbsLoader 추가

---
+++ src/com/kyuwan/bbs/model/BbsLoader.java
@@ -0,0 +1,16 @@
+package com.kyuwan.bbs.model;
+
+public class BbsLoader {
+
+	public String read(){
+		String result = "";
+		
+		// TODO
+		
+		return result;
+	}
+	public void write(){
+		
+	}
+	
+}
