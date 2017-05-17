e0ca384d8b8515d6a743a7a30ecf14cd113150d3
kyuwankim <lpnids@gmail.com>
2017-05-17 11:44:03 +0900

    목록에대한 기본설계

--- src/com/kyuwan/bbs/view/BbsList.java
+++ src/com/kyuwan/bbs/view/BbsList.java
@@ -2,4 +2,29 @@ package com.kyuwan.bbs.view;
 
 public class BbsList {
 
+	// 목록 보여주기
+	public void showList(){
+		
+	}
+	
+	// 쓰기이동 
+	public void goInput(){
+		
+	}
+	
+	// 상세보기 이동 
+	public void goDetail(long id){
+		
+	}
+	
+	// 검색
+	public void search(String word){
+		
+	}
+	
+	// 삭제
+	public void delete(long id){
+		
+	}
+	
 }
